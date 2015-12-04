d1cc920e771cd3899018190c72f6288bd2e2c145
Rayer <rayer.tung@ruckuswireless.com>
2015-12-04 16:06:26 +0800

    Fix a issue that version_list point to invalid link

--- configuration.py
+++ configuration.py
@@ -6,7 +6,7 @@ This section stores data the same among all nodes.
 
 file_map = (
     ('deploy.py', '/usr/bin/deploy'),
-    ('version_check.py', '/usr/bin/version_check'),
+    ('version_list.py', '/usr/bin/version_list'),
     ('sesame2.py', '/usr/bin/sesame2'),
     ('vmmanage.py', '/usr/bin/vmmanage')
 )
